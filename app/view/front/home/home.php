dd088557642a33e60d615040999f3917ccf36869
Ruli Royana Widura <ruliroyanawidura@gmail.com>
2021-06-22 14:04:18 +0700

    FEAT: Home page

--- app/view/front/home/home.php
+++ app/view/front/home/home.php
@@ -19,27 +19,90 @@
     color:#ffffff;
     font-size: 10px;
 }
+.alamat{
+    background: #fff;
+    border: 1px solid #bdc7d8;
+    box-sizing: border-box;
+    cursor: text;
+    font-family: Helvetica, Arial, sans-serif;
+    font-size: 11px;
+    padding: 3em;
+}
+.juli{
+  border-bottom: 1px dotted #dedede;
+  padding-bottom: 2em;
+  font-style: helvetica;
+}
+.kom{
+
+}
+.urut{
+  padding-left:25em;
+}
 </style>
-<div class="row">
+<div class="row" style="margin:0px;">
   <div class="col-md-12 artikel">
     <h1>ARTIKEL</h1>
   </div>
 </div>
 
-<div class="row">
+<div class="row" style="margin:0px;">
   <div class="col-md-12 lembaga">
     <h1>Lembaga Kemasyarakatan</h1>
   </div>
 </div>
 
-<div class="row">
-  <div class="col-md-4">
-    <h5>29 Juni 2013 18:38:33</h5>
+<div class="row" style="margin:0px;">
+  <div class="col-md-12 juni">
+    <a href=""><i class="fa fa-user" aria-hidden="true"> 29 Juni 2013 18:38:33</i></a>
+    <a href=""><i class="fa fa-user" aria-hidden="true"> Adminiatrator</i></a>
+    <a href=""><i class="fa fa-eye" aria-hidden="true"> 87 Kali Dibaca</i></a>
+    <a href=""><i class="fa fa-thumbs-up" aria-hidden="true"> Suka 5</i></a>
+    <a href=""> Bagikan</a>
   </div>
-
-  <div class="col-md-4">
+</div>
+<div class="row" style="margin:0px;">
+  <div class="col-md-12 juli">
   </div>
+</div>
+<div class="row" style="margin:0px;">
+  <div class="col-md-12">
+    <h3>Berisi data lembaga yang ada di desa beserta deskripsi dan susunan pengurusnya</h3>
+  </div>
+</div>
 
-  <div class="col-md-4">
+<div class="row" style="margin:0px;">
+  <div class="col-md-12">
+    <a href="" style=""><i class="fa fa-facebook-official fa-4x" aria-hidden="true"></i></a>
+    <a href="" style="color:#5bc0de; padding-left:10px;"><i class="fa fa-twitter-square fa-4x" aria-hidden="true"></i></a>
+    <a href="" style="color:#d9534f; padding-left:10px;"><i class="fa fa-comments-o fa-4x" aria-hidden="true"></i></a>
+    <a href="" style="color:#efefef; padding-left:10px;"><i class="fa fa-paper-plane fa-4x" aria-hidden="true"></i></a>
+    <a href="" style="color:#f0ad4e; padding-left:10px;"><i class="fa fa-floppy-o fa-4x" aria-hidden="true"></i></a>
+    <a href="" style="color:#5cb85c; padding-left:10px;"><i class="fa fa-whatsapp fa-4x" aria-hidden="true"></i></a>
+    <br />
+    <br /><br /><br />
+  </div>
+</div>
+<div class="row" style="margin:0px;">
+  <div class="col-md-6 kom">
+    <h4><b>0 Komentar</b></h4>
+
+  </div>
+  <div class="col-md-6 urut">
+    <h4>Urut Berdsarkan</h4>
+  </div>
+</div>
+<br />
+<br />
+<div class="row" style="margin:0px;">
+  <div class="col-md-12 juli">
+  </div>
+</div>
+<div class="row" style="margin:0px;">
+  <div class="col-md-3">
+    <img src="img_girl.jpg" alt="Girl in a jacket" width="500" height="600">
+  </div>
+  <div class="col-md-9">
+
   </div>
 </div>
